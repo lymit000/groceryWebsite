87b82a3bc6a2f8a8ccda338844f07c5c1bc700a3
mitch <lymit000@uw.edu>
2022-01-30 17:11:30 -0800

    initial

--- config/[...date_id].js
+++ config/[...date_id].js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://vercel.com/lymit000/grocery';
+export const server = dev ? 'http://localhost:3000' : 'https://grocery-od7qlad1d-lymit000.vercel.app';
