49919d1affddfe7d2a948d8a5aa6c2fb9593682c
Mitchell Ly <mitchellly@Mitchells-MacBook-Pro.local>
2022-03-29 23:14:25 -0700

    serv

--- config/index.js
+++ config/index.js
@@ -1,3 +1,9 @@
 const dev = process.env.NODE_ENV !== 'production';
-
+export default function handler(request, response) {
+    response.status(200).json({
+        body: request.body,
+        query: request.query,
+        cookies: request.cookies,
+    });
+}
 export const server = dev ? 'http://localhost:3000/' : 'https://grocerycostco.vercel.app';
